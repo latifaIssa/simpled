b36f6516ef61ea649f3a8e9fdc0a7967d6886dfe
latifaIssa <latifa.masri1998@gmail.com>
2020-08-10 22:11:45 +0300

    complete download extension

--- src/loader.js
+++ src/loader.js
@@ -1,11 +1,24 @@
 s = document.createElement("script");
 
-s.src = chrome.extention.getURL("src/youtubedl.js");
+s.src = chrome.extension.getURL("src/youtubedl.js");
 
-// s.setAttribute("data-ext", chrome.runtime.id);
+s.setAttribute("data-ext", chrome.runtime.id);
+s.id = "vdyt";
 
 (document.head || document.createElement).appendChild(s);
 
 window.addEventListener("message", function(e) {
-    console.log(e.data.type);
+    // console.log(e.data.type);
+    // console.log(e.sender);
+    console.log("download", e);
+    var ext = e.data.type.split('/')[1].split(";")[0];
+    var fn = e.data.name +"."+ ext;
+    console.log(fn, chrome);
+
+    chrome.runtime.sendMessage({name: fn, url:e.data.url}, function(res){
+        console.log(res);
+    });
+
+    // chrome.downloads.download({url: e.data.url, filename: fn});
+
 })
--- src/youtubedl.js
+++ src/youtubedl.js
@@ -9,20 +9,47 @@ function downloadVideo() {
         dl.className += "shown";
     }
 
-    var data = { "type": "download clicked" };
+    // var ext_id = document.getElementById("vdyt").getAttribute("data-ext");
+    // console.log(ext_id);
+
+    // var data = { "type": "download clicked" };
+    // window.postMessage(data, "*");
+}
+
+function downloadURI(event){
+    event.preventDefault();
+    event.stopPropogation();
+
+    var url = event.currentTarget.getAttribute("href");
+    var name = document.getElementByTagName("title").innerText;
+    var datatype = event.currentTarget.getAttribute("data-type");
+    var data = {url:url, name: name, sender:"YTDL", type: datatype};
+
     window.postMessage(data, "*");
+
+    var dl = document.getElementById("videoDownloadDropdown");
+
+    if (dl.className.indexOf("show") > -1) {
+        dl.className = dl.className.replace("shown", "");
+
+    } else {
+        dl.className += "shown";
+    }
+
+    return false;
+
 }
 
 // windows.onload = function() {
-var videoUrls = window.ytplayer.config.args.url_encoded_fmt_stream_map.split(',').map(function(item) {
-    return item.split("&").reduce(function(pre, cur) {
-        cur = cur.split('=');
-        return Object.assign(pre, {
-            [cur[0]]: decodeURIComponent(cur[1])
-        });
-    }, {});
-});
-console.log('Our extension has loaded', videoUrls);
+// var videoUrls = window.ytplayer.config.args.url_encoded_fmt_stream_map.split(",").map(function(item) {
+//     return item.split("&").reduce(function(pre, cur) {
+//         cur = cur.split('=');
+//         return Object.assign(pre, {
+//             [cur[0]]: decodeURIComponent(cur[1])
+//         });
+//     }, {});
+// });
+// console.log('Our extension has loaded', videoUrls);
 // }
 
 
@@ -34,41 +61,45 @@ console.log('Our extension has loaded', videoUrls);
 
 
 
+var videoUrls = {};
+(async() => {
+    const html = await fetch(window.location.href).then((resp) => resp.text()).then((text) => text);
+    const startStr = 'ytplayer.config = {';
+    const start = html.indexOf(startStr) + startStr.length - 1;
+    const end = html.indexOf('};', start) + 1;
+    const playerObj = JSON.parse(html.slice(start, end));
 
-// (async() => {
-//     const html = await fetch(window.location.href).then((resp) => resp.text()).then((text) => text);
-//     const startStr = 'ytplayer.config = {';
-//     const start = html.indexOf(startStr) + startStr.length - 1;
-//     const end = html.indexOf('};', start) + 1;
-//     const playerObj = JSON.parse(html.slice(start, end));
+    playerObj.args.player_response = JSON.parse(playerObj.args.player_response);
 
-//     playerObj.args.player_response = JSON.parse(playerObj.args.player_response);
+    videoUrls = playerObj.args.player_response.streamingData.adaptiveFormats.reduce((acc, item) => {
+        if (!acc[item.quality]) {
+            acc[item.quality] = {};
+        }
 
-//     const videoUrls = playerObj.args.player_response.streamingData.adaptiveFormats.reduce((acc, item) => {
-//         if (!acc[item.quality]) {
-//             acc[item.quality] = {};
-//         }
+        const mimeType = item.mimeType.split(';')[0];
 
-//         const mimeType = item.mimeType.split(';')[0];
+        acc[item.quality][mimeType] = item;
 
-//         acc[item.quality][mimeType] = item;
+        return acc;
+    }, {});
 
-//         return acc;
-//     }, {});
-
-//     console.log('Our extension has loaded', videoUrls);
-// })();
+    console.log('Our extension has loaded', videoUrls);
+})();
 
 
-var container = document.getElementById("menu-container");
-var btn = document.createElement("button");
+var container = document.getElementById("top-level-buttons");
+var btn = document.createElement("ytd-toggle-button-renderer");
 btn.className = "style-scope ytd-button-renderer style-default size-default";
 btn.setAttribute('role', 'button');
 btn.id = "videoDownloadDropdown";
 btn.innerText = "Download";
 
+var dropdown = document.createElement("div");
+dropdown.id = "videoDownloadDropdown";
+container.appendChild(dropdown);
+
 var dropList = document.createElement("ul");
-dropList.appendChild(dropList);
+dropdown.appendChild(dropList);
 
 
 
@@ -80,7 +111,16 @@ container.appendChild(btn);
 // btn.addEventListener("click", downloadVideo);
 for (i in videoUrls) {
     var item = document.createElement("a");
-    item.innerText = videoUrls[i]["quality"];
+    var ext = videoUrls[i]["type"].split('/')[1].split(";")[0];
+    
+    item.innerText = videoUrls[i]["quality"] + "(" + ext + ")";
+
     item.setAttribute("href", videoUrls[i]["url"]);
+    item.setAttribute("target", "_blank");
+    item.setAttribute("data_type", videoUrls[i]["type"]);
+    item.setAttribute("click", downloadURI);
+
     dropList.appendChild(item);
 }
+
+btn.addEventListener("click", downloadVideo);
